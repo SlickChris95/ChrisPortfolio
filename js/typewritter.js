ce899afd090b5c6bc9fa54f9f213a414bd057a3a
Chris Torres <torres.christian33@yahoo.com>
2019-10-23 16:02:27 -0700

    replaced profile image, added typewritter effect

---
+++ js/typewritter.js
@@ -0,0 +1,49 @@
+// var pos = 0;
+// var speed = 250;
+// var str = document.getElementById('str').innerText;
+// console.log(strText)
+//
+// //empty the str from span
+// document.getElementById('str').innerText = '';
+//
+// //typing fn
+// // function type() {
+// //   if(pos < str.length){
+// //     document.getElementById('str').innerText += str.charAt(pos);
+// //     pos++;
+// //     setTimeout(type,speed); //call this fn again to type all letters
+// //   }
+// // }
+//
+// setTimeout(type,speed);
+var pos = 0;
+var turn = 0;
+var data = ['Web Developer','Leader','Problem Solver'];
+var speed = 200;
+
+setTimeout(typeWriter, speed);
+
+function typeWriter() {
+  if (pos < data[turn].length) {
+    document.getElementById("demo").innerHTML += data[turn].charAt(pos);
+    console.log(data[turn].charAt(pos));
+    pos++;
+    setTimeout(typeWriter, speed);
+  } else {
+  	setTimeout(erase, speed+100);
+  }
+}
+
+function erase() {
+	if (pos >= 0) {
+      var str=data[turn].toString().substring(0, pos);
+      document.getElementById("demo").innerHTML = str;
+      pos--;
+      setTimeout(erase, speed-100);
+    } else {
+      turn++;
+      if(turn>=data.length)
+        turn=0;
+      setTimeout(typeWriter, speed);
+    }
+}
